764c5b14431468af180afaa9c36fdebcbb7bf898
MdSakibUddin <md.sakib.uddin@g.bracu.ac.bd>
2021-09-26 23:55:02 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <h1 className="header-sizing"> New Goru Bazar</h1>
+                <h2> Buying limit : 600000 TK Total</h2>
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default Header;
